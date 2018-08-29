3b1428f1ba027c696e9189e7cfb2813b3846ff75
Yeonji Cho <wjalice07@gmail.com>
2018-08-29 08:15:19 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request
+import random
+import csv
+
+#names = []
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+    
+@app.route("/result")
+def result():
+    # 한글 주석 써도 에러 안남.
+    name1 = request.args.get('name1')
+    name2 = request.args.get('name2')
+    match = random.randrange(50, 101)
+    '''names라는 배열에 입력된 두 이름을 넣는다.
+    names.append(name1)
+    names.append(name2)'''
+    
+    # 'names.csv'파일을 만들어서 저장한다.
+    f = open('names.csv', 'a+')
+    a = csv.writer(f)
+    a.writerow([name1,name2])
+    f.close()
+    return render_template('result.html',name1=name1,name2=name2,match=match)
+    
+@app.route("/admin")
+def admin():
+    # names에 들어가 있는 모든 이름을 출력한다.
+    f = open('names.csv', 'r')
+    rr = csv.reader(f)
+    names = rr
+    return render_template('admin.html', names=names)
